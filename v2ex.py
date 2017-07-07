e889a138523a0fc63d855c0fc63eb3617b8ba4b3
李春光 <1192126986@qq.com>
2017-07-07 18:50:09 +0800

    mysql pymongo mongoengine 存储数据

--- v2ex.py
+++ v2ex.py
@@ -1,26 +1,55 @@
 from pyspider.libs.base_handler import *
 import random
-import MySQLdb
+# import MySQLdb
+import pymongo
+from mongoengine import *
+
+'''
+方法2： pymongo 连接 mongodb数据库
+
+# 连接mongodb
+# def get_coll():
+#     client = pymongo.MongoClient('127.0.0.1', 27017)
+#     db = client.spider
+#     coll = db.spider_coll
+#     return coll
+'''
+
+'''
+方法3: mongoengine连接mongodb数据库
+'''
+connect('v2ex')  # mongoengine连接mongodb
+
+
+class Question(Document):
+    title = StringField()
+    content = StringField()
+
+    def __str__(self):
+        return '<Question %s>'%self.title
 
 
 class Handler(BaseHandler):
     crawl_config = {
 
     }
+    '''
+    方法1： 连接mysql数据库
 
-    def __init__(self):
-        self.db = MySQLdb.connect('localhost','root', 'workhard', 'wenda', charset='utf-8')
-
-    def add_question(self,title, content):
-        try:
-            cursor = self.db.cursor()
-            sql = 'insert into question(title, content, user_id, created_date, comment_count) values ("%s","%s",%d, %s, 0)' % (
-                title, content, random.randint(1, 10), 'now()')
-            cursor.execute(sql)
-            self.db.commit()
-        except Exception as e:
-            print(e)
-            self.db.roolback()
+    # def __init__(self):
+    #     self.db = MySQLdb.connect('localhost','root', 'workhard', 'wenda', charset='utf-8')
+    #
+    # def add_question(self,title, content):
+    #     try:
+    #         cursor = self.db.cursor()
+    #         sql = 'insert into question(title, content, user_id, created_date, comment_count) values ("%s","%s",%d, %s, 0)' % (
+    #             title, content, random.randint(1, 10), 'now()')
+    #         cursor.execute(sql)
+    #         self.db.commit()
+    #     except Exception as e:
+    #         print(e)
+    #         self.db.roolback()
+    '''
 
     @every(minutes=24 * 60)
     def on_start(self):
@@ -49,7 +78,16 @@ class Handler(BaseHandler):
     def detail_page(self,response):
         title = response.doc('h1').text()
         content = response.doc('div.topic_content').html().replace('"', '\"')
-        self.add_question(title,content)
+        # 方法1
+        # self.add_question(title,content)
+        # 方法2
+        # question = {'title': title, 'content': content}
+        # coll =get_coll()
+        # coll.insert(question)
+        #方法3
+        question = Question(title=title, content=content)
+        question.save()
+
         return {
             'url': response.url,
             'title': title,
